f86c459f45420b9bd1ecdf7fbff99dd99f8ec768
yuanj <yuanj@DESKTOP-4UOV53P>
2017-04-13 12:59:35 -0700

    Add project files.

---
+++ Homework1/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SubmitForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SubmitForm(Models.GuestResponse guestResponse)
+        {
+            if(ModelState.IsValid)
+            {
+                return View("SentCard", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Homework1/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Homework1.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage ="Please enter From")]
+        public string FromName { get; set; }
+
+        [Required(ErrorMessage = "Please enter To")]
+        public string ToName { get; set; }
+
+        [Required(ErrorMessage = "Please enter Message")]
+        public string Message { get; set; }
+
+    }
+}
